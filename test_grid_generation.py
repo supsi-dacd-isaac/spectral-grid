37267d47a052320dcf326f7d89fe7a4d29dc8ca3
federico.rosato@supsi.ch <federico.rosato@supsi.ch>
2022-08-10 11:06:24 +0200

    Updated grid generation test script and requirements.txt

--- test_grid_generation.py
+++ test_grid_generation.py
@@ -1,6 +1,7 @@
-from mbg import MapBoxGraph
 import matplotlib
-from time import time
+
+from spectral_grid import MapBoxGraph
+
 matplotlib.use('Qt5Agg')
 
 if __name__ == '__main__':
@@ -14,6 +15,6 @@ if __name__ == '__main__':
         'node_shape': 'h',
     }
 
-    mbg = MapBoxGraph(AROSIO, log_level=10)
+    mbg = MapBoxGraph(AROSIO, None, log_level=10)
     mbg.compute([.25, .35, .4], maxiter=100, imbalance_tol=1e-1)
     mbg.subplot(**GRAPHIC_OPTS)
